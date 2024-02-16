dba0913ad782efa3d4172a5702ad541f0518223a
Mihai Masala <mihaimasala@gmail.com>
2024-02-17 01:15:46 +0200

    Integrate roorca into rosft

--- ft_datasets/rosft_dataset.py
+++ ft_datasets/rosft_dataset.py
@@ -4,6 +4,7 @@ from conversations_dataset import get_preprocessed_conversations_dataset
 from rodolly_dataset import get_preprocessed_rodolly_dataset
 from roselfinstruct_dataset import get_preprocessed_roselfinstruct_dataset
 from ronorobots_dataset import get_preprocessed_ronorobots_dataset
+from roorca_dataset import get_preprocessed_roorca_dataset
 
 SPLIT = "test"
 
@@ -13,8 +14,9 @@ def get_preprocessed_rosft_dataset(dataset_config, tokenizer, split):
     rodolly_dataset = get_preprocessed_rodolly_dataset(dataset_config, tokenizer, split)
     roselfinstruct_dataset = get_preprocessed_roselfinstruct_dataset(dataset_config, tokenizer, split)
     ronorobots_dataset = get_preprocessed_ronorobots_dataset(dataset_config, tokenizer, split)
-    
-    sft_dataset = datasets.concatenate_datasets([roalpaca_dataset, rodolly_dataset, roselfinstruct_dataset, ronorobots_dataset])
+    roorca_dataset = get_preprocessed_roorca_dataset(dataset_config, tokenizer, split)
+
+    sft_dataset = datasets.concatenate_datasets([roalpaca_dataset, rodolly_dataset, roselfinstruct_dataset, ronorobots_dataset, roorca_dataset])
     sft_dataset = sft_dataset.shuffle(seed=42)
     return sft_dataset
 
@@ -30,14 +32,17 @@ if __name__ == "__main__":
     rodolly_dataset = get_preprocessed_rodolly_dataset(None, tokenizer, SPLIT)
     roselfinstruct_dataset = get_preprocessed_roselfinstruct_dataset(None, tokenizer, SPLIT)
     ronorobots_dataset = get_preprocessed_ronorobots_dataset(None, tokenizer, SPLIT)
-    
+    roorca_dataset = get_preprocessed_roorca_dataset(None, tokenizer, SPLIT)
+
     # print(convs_dataset)
     print(roalpaca_dataset)
     print(rodolly_dataset)
     print(roselfinstruct_dataset)
     print(ronorobots_dataset)
+    print(roorca_dataset)
 
-    sft_dataset = datasets.concatenate_datasets([roalpaca_dataset, rodolly_dataset, roselfinstruct_dataset, ronorobots_dataset])
+
+    sft_dataset = datasets.concatenate_datasets([roalpaca_dataset, rodolly_dataset, roselfinstruct_dataset, ronorobots_dataset, roorca_dataset])
     print(sft_dataset)
 
     
